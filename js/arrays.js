c8b3a031c030f3eb00e2cd9f29813879ecf808bc
Anvay Arora <75296875+the-protagonist-view@users.noreply.github.com>
2022-11-20 18:35:48 +0530

    Update arrays.js

--- js/arrays.js
+++ js/arrays.js
@@ -1,6 +1,6 @@
 let founders = [
     {
-        img: "https://avatars.githubusercontent.com/u/67160842?v=4",
+        img: "https://media-exp1.licdn.com/dms/image/D4D03AQEvEShnwftB-g/profile-displayphoto-shrink_200_200/0/1668762219213?e=1674691200&v=beta&t=1M2PbAMmXdfcA_emxxfO6Q09hP3735uAsrjL3z-F1P4",
         name: "Anvay Arora",
         role: "Co-Founder",
         social: {
@@ -14,30 +14,40 @@ let founders = [
         name: "Sparsh Agarwal",
         role: "Co-Founder",
         social: {
-            instagram: "",
-            github: "",
-            mail: ""
+            instagram: "https://www.instagram.com/pablo0chocobar",
+            github: "a",
+            mail: "meetsparsh4@gmail.com"
         }
     },
     {
-        img: "https://avatars.githubusercontent.com/u/67160842?v=4",
+        img: "https://media-exp1.licdn.com/dms/image/C4D03AQGlkwD6-V8p8w/profile-displayphoto-shrink_200_200/0/1661838003945?e=1674691200&v=beta&t=_KwN5Aqx7l7yI2MCwYrJlMp4aG-pk2ecH00_GP5sC5Q",
         name: "Arnav Sarda",
         role: "Co-Founder",
         social: {
-            instagram: "",
-            github: "",
-            mail: ":"
+            instagram: "https://www.instagram.com/airnairv",
+            github: "a",
+            mail: "asarda04@gmail.com"
         }
 
     },
     {
-        img: "https://avatars.githubusercontent.com/u/67160842?v=4",
+        img: "https://media-exp1.licdn.com/dms/image/C4E03AQHdQnTQX5FjGA/profile-displayphoto-shrink_200_200/0/1652425772833?e=1674691200&v=beta&t=5bMAIG2mfvEXUdI0dBJnvSRycPaeH2WrBNQrWAo20Fs",
         name: "Kanishk Jain",
         role: "Co-Founder",
         social: {
-            instagram: "",
-            github: "",
-            mail: ""
+            instagram: "https://www.instagram.com/kjpromager10",
+            github: "a",
+            mail: "kj.promager10@gmail.com"
+        }
+    }
+    {
+        img: "https://pps.whatsapp.net/v/t61.24694-24/306941897_5797827643617233_6158692481151390474_n.jpg?ccb=11-4&oh=01_AdRdSnFVgTWI26dYZ4lyup8mM6qYAEowVkyVAk8FenBBDQ&oe=63874B43",
+        name: "Panshul Kantan",
+        role: "Co-Founder",
+        social: {
+            instagram: "a",
+            github: "a",
+            mail: "panshulkantansrivastava@gmail.com"
         }
     }
 ]
